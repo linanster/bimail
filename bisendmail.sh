a80fcc28f8c241faf8ab7f2626a2fea5e5046291
nan@gecloud3 <believelinan@aliyun.com>
2021-01-12 07:54:48 +0000

    bisendmail.sh: add logan and tom

--- bisendmail.sh
+++ bisendmail.sh
@@ -7,8 +7,10 @@ date=$(date +%F)
 subject="BI Dashboard Database Daily Report ${date}"
 from='nan2.li@ge.com'
 # tomail='nan2.li@ge.com,'
+# tomail='nan2.li@ge.com,believelinan@aliyun.com'
 # tomail='zhe.hu@ge.com,'
-tomail='Zhiyong.Wang@ge.com,zhe.hu@ge.com,blake.xue@ge.com,nan2.li@ge.com'
+# tomail='Zhiyong.Wang@ge.com,zhe.hu@ge.com,blake.xue@ge.com,nan2.li@ge.com'
+tomail='Zhiyong.Wang@ge.com,zhe.hu@ge.com,blake.xue@ge.com,nan2.li@ge.com,logan.wanner@ge.com,tom.stimac@ge.com'
 ccmail='nan2.li@ge.com,blake.xue@ge.com'
 attach1='data/overview.txt'
 attach2="data/device.txt"
@@ -16,8 +18,9 @@ attach3="data/device_type_format.txt"
 attach4="data/cbyge_devices_commissioned.txt"
 body="data/body.txt"
 
-echo **Query Time: $(date -d "+8 hours" "+%F %T")** > "${body}"
-# echo >> "${body}"
+# echo **Query Time: $(date -d "+8 hours" "+%F %T")** > "${body}"
+echo **Query Time: $(date -d "+8 hours" "+%F %T") \(China Standard Time\) $(date -d "-8 hours" "+%F %T") \(Pacific Standard Time\)** > "${body}"
+echo >> "${body}"
 echo '**Data source: mysql -hestar.xlink.cloud -uroot -pGEMysql20200 GE**' >> "${body}"
 echo >> "${body}"
 echo '**This is automatic mail from SES (Amazon Simple Email Service) !**' >> "${body}"
